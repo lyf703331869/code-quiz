165a3cab36321dff74eb7010b3ffdbc57e5d5307
Yafei Liu <yafeiliu@yafeis-air.myfiosgateway.com>
2022-05-16 10:01:52 -0400

    Add check function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,26 +3,77 @@ var startPage = document.querySelector(".startPage");
 var startBtn = document.querySelector(".start");
 var quizQuestion = document.querySelector(".quizQuestion");
 var timeEl = document.querySelector(".timer");
-var timeLeft = 100;
+var ansReveal = document.querySelector(".answerReveal");
+var enterInitials = document.querySelector(".enter-initials");
+var timeLeft = 70;
 var i = 0;
 
+// console.log(questionShowUp[i].answers[0]);
+
 var questionChar1 = {
-  question: "What is one plus one?",
-  answers: ["two", "three", "four", "five"],
-  correctAnswer: "two",
+  question: "Commonly used data types do NOT include:",
+  answers: ["1 - Booleans", "2 - Alerts", "3 - Strings", "4 - Numbers"],
+  correctAnswer: "2 - Alerts",
 };
 
 var questionChar2 = {
-  question: "What is one minus one?",
-  answers: ["zero", "one", "two", "three"],
-  correctAnswer: "zero",
+  question: "The condition of an if/else statement is enclosed within ______.",
+  answers: [
+    "1 - Quotes",
+    "2 - Curly Brackets",
+    "3 - Parentheses",
+    "4 - Square Brackets",
+  ],
+  correctAnswer: "3 - Parentheses",
 };
 
-var questionShowUp = [questionChar1, questionChar2];
+var questionChar3 = {
+  question: "Arrays in Javascript can be used to store ______.",
+  answers: [
+    "1 - Numbers and strings",
+    "2 - Other Arrays",
+    "3 - Booleans",
+    "4 - All of the above",
+  ],
+  correctAnswer: "4 - All of the above",
+};
 
-console.log(questionShowUp);
+var questionChar4 = {
+  question:
+    "String values must be enclosed within ______ when being assigned to variables.",
+  answers: [
+    "1 - Quotes",
+    "2 - Curly Brackets",
+    "3 - Commas",
+    "4 - Parentheses",
+  ],
+  correctAnswer: "1 - Quotes",
+};
+
+var questionChar5 = {
+  question:
+    "A very useful tool used during development and debugging for printing content to the debugger is:",
+  answers: [
+    "1 - Javascript",
+    "2 - console.log",
+    "3 - Terminal/bash",
+    "4 - For loops",
+  ],
+  correctAnswer: "2 - console.log",
+};
+
+var questionShowUp = [
+  questionChar1,
+  questionChar2,
+  questionChar3,
+  questionChar4,
+  questionChar5,
+];
 
 startBtn.addEventListener("click", function () {
+  startPage.style.display = "none";
+  quizQuestion.style.display = "";
+  quizQuestions();
   var timeInterval = setInterval(function () {
     timeLeft--;
     timeEl.textContent = timeLeft + "s left";
@@ -32,22 +83,19 @@ startBtn.addEventListener("click", function () {
       // InputYourName();
     }
   }, 1000);
-  startPage.style.display = "none";
-  quizQuestion.style.display = "";
-  quizQuestions();
 });
 
 function quizQuestions() {
-  if (i <= 10) {
+  if (i <= 4) {
     loadQuestion();
+    // console.log(questionShowUp[i]);
     checkIfCorrect();
   } else {
-    //   InputYourName();
+    InputYourName();
   }
 }
 
 function loadQuestion() {
-  // need to find out how to change questions 5/14
   quizQuestion.children[0].textContent = questionShowUp[i].question;
   quizQuestion.children[1].children[0].textContent =
     questionShowUp[i].answers[0];
@@ -60,24 +108,41 @@ function loadQuestion() {
 }
 
 function checkIfCorrect() {
-  var answerChosen = document.querySelector(".quizQuestion");
-  answerChosen.addEventListener("click", function (event) {
+  quizQuestion.addEventListener("click", function (event) {
     var element = event.target;
-
+    console.log(element);
     if (
-      element.matches("button") &&
+      element.matches(".button") &&
       questionShowUp[i].correctAnswer === event.target.textContent
     ) {
       i++;
       quizQuestions();
+      ansReveal.style.display = "";
+      ansReveal.children[1].textContent = "Correct!";
+      showAnswer();
     } else {
       i++;
-      quizQuestions();
       timeLeft -= 10;
+      quizQuestions();
+      ansReveal.style.display = "";
+      ansReveal.children[1].textContent = "Wrong!";
+      showAnswer();
     }
   });
 }
-// function InputYourName(event){
-//     event.preventDefault();
-//     var userName =
-//
+function showAnswer() {
+  var j = 2;
+  var answerInterval = setInterval(function () {
+    clearInterval(answerInterval);
+    ansReveal.children[1].textContent = "";
+    ansReveal.style.display = "none";
+  }, 1000);
+}
+
+function InputYourName() {
+  // event.preventDefault();
+  clearInterval(timeInterval);
+  quizQuestion.style.display = "none";
+  enterInitials.style.display = "";
+  enterInitials.children[1].textContent = "Your final score is " + timeLeft;
+}
